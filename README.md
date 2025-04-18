fbe67c98456ec537aff29c69d8f24b26f5c9d008
bipulkuri <bkk9893@nyu.edu>
2025-04-17 23:22:55 -0400

    pretty done

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # github-actions-demo
 
-
 Simple node.js app that servers "hello world" and demos github-actions
 
 Fork the repo to get the code and follow allong
--- index.test.js
+++ index.test.js
@@ -1,9 +1,9 @@
-const request = require('supertest');
-const app = require('./index.js'); // Assuming you export `app` from index.js
+const request = require("supertest");
+const app = require("./index.js"); // Assuming you export `app` from index.js
 
-describe('GET /', () => {
-  it('should return Hello, World!', async () => {
-    const response = await request(app).get('/');
-    expect(response.text).toBe('Hello, World!');
+describe("GET /", () => {
+  it("should return Hello, World!", async () => {
+    const response = await request(app).get("/");
+    expect(response.text).toBe("Hello, World!");
   });
 });
